2d88f5b78cea906182949dde53bbcf15df71f9e6
Scott Werner <stwerner@vt.edu>
2019-12-04 23:34:51 -0500

    Day 4

---
+++ 2019/4/solution.rb
@@ -0,0 +1,34 @@
+require "pry"
+
+def two_adjacent_same(number)
+  number[0] == number[1] ||
+  number[1] == number[2] ||
+  number[2] == number[3] ||
+  number[3] == number[4] ||
+  number[4] == number[5]
+end
+
+def never_decrease(number)
+  number[0] <= number[1] &&
+  number[1] <= number[2] &&
+  number[2] <= number[3] &&
+  number[3] <= number[4] &&
+  number[4] <= number[5]
+end
+
+def meets_criteria(number)
+  number = number.split("").map(&:to_i)
+  two_adjacent_same(number) && never_decrease(number)
+end
+
+count = 0
+
+(152085..670283).each do |number|
+  count = count + 1 if meets_criteria(number.to_s)
+end
+
+# puts count
+
+# puts meets_criteria("111111") == true
+# puts meets_criteria("223450") == false
+# puts meets_criteria("123789") == false
